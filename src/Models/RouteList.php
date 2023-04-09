afea3ab22dd3af917edfdad9419d3daff5f000df
kite.lin <kite.lin@blackwellglobal.com>
2023-04-10 01:21:51 +0800

    update for Laravel 6.4.1

--- src/Models/RouteList.php
+++ src/Models/RouteList.php
@@ -2,12 +2,10 @@
 
 namespace Lkh\RouteList\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class RouteList extends Model
 {
-    use HasFactory;
     protected $table = 'route_list';
     protected $fillable = ['id','created_at','updated_at','methods','domain','path','name','action','middleware'];
 }
